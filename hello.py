c0144ae6a13863b2ce7273ab01edf8d07556141d
Anton <a.tosick@gmail.com>
2019-07-13 01:22:35 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def app(environ, start_responce):
 	headers = [
 		('Content-Type', 'text/plain')
 	]
-	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+	body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
 	start_responce(status, headers)
 	return [body]
