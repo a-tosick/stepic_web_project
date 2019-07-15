17740e33ce8ab3acd7b5aa6ed9c59dd80d70e93c
a-tosick_box <a.tosick@gmail.com>
2019-07-15 23:39:01 +0300

    update all conf

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
 gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
