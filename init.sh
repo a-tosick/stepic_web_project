284825a0437ceda04c74088c6724c33ed31fc3b9
a-tosick_box <a.tosick@gmail.com>
2019-07-15 22:42:44 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
+gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
+gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application &
