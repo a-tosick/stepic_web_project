a366ebc483f081b6fb52666bff51f51a85590073
a-tosick_box <a.tosick@gmail.com>
2019-07-16 00:22:27 +0300

    update ask

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from qa.views import test
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', include('qa.urls')),
+	url(r'^login/', include('qa.urls')),
+	url(r'^signup/', include('qa.urls')),
+	url(r'^question/0001/', include('qa.urls')),
+	url(r'^ask/', include('qa.urls')),
+	url(r'^popular/', include('qa.urls')),
+	url(r'^new/', include('qa.urls')),
 ]
