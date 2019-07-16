afd6ca87fd7d8b22d02836f3a41e162ecf43ea73
a-tosick_box <a.tosick@gmail.com>
2019-07-16 21:12:32 +0300

    return to previos configs

--- ask/qa/urls.py
+++
@@ -1,5 +0,0 @@
-from ask.views import urlpatterns
-
-urlpatterns = patterns('ask.views',
-    url(r'^', 'qa.views.test', name='test'),
-)
--- init.sh
+++ init.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-sudo rm /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+
 sudo /etc/init.d/nginx restart
-#sudo service nginx restart
-#gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
-#gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application
