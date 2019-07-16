5fbc74f4117515845ba2067d4dd277690badbdac
a-tosick_box <a.tosick@gmail.com>
2019-07-17 00:39:19 +0300

    update some files

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,5 @@
 from django.conf.urls import include, url
 
 urlpatterns = [ 
-	url(r'^$', include('qa.urls')), 
-	url(r'^question/(\d+)/$', include('qa.urls')),
-	url(r'^login/.*$', include('qa.urls')), 
-	url(r'^signup/.*$', include('qa.urls')), 
-	url(r'^ask/.*$', include('qa.urls')), 
-	url(r'^popular/.*$', include('qa.urls')), 
-	url(r'^new/.*$', include('qa.urls')), 
+	url(r'^$', views.test, name="index"),
 ]
