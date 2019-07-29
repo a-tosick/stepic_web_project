e8b5b261b087826e7f9b229fb14fff2502bc37b6
a-tosick_box <a.tosick@gmail.com>
2019-07-30 01:29:54 +0300

    update models

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stepic_web',
+        'USER': 'root', # root for my PC, box for tests on stepic
     }
 }
 
